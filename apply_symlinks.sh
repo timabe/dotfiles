43c2d59eb4424ff08122be5d0ae92e506422b8ab
Tim Abraham <tabraham@expa.com>
2017-10-18 16:26:06 -0700

    fixing bug where I omitted add a space in the file check if statements

--- apply_symlinks.sh
+++ apply_symlinks.sh
@@ -2,19 +2,19 @@
 
 cd $HOME
 
-if [-f .screenrc ]; then
+if [ -f .screenrc ]; then
   mv .screenrc .screenrc~
 fi
 
 ln -s dotfiles/.screenrc .
 
-if [-f .bash_profile ]; then
+if [ -f .bash_profile ]; then
   mv .bash_profile .bash_profile~
 fi
 
 ln -s dotfiles/.bash_profile .
 
-if [-f .bashrc ]; then
+if [ -f .bashrc ]; then
   mv .bashrc .bashrc~
 fi
 
@@ -22,7 +22,7 @@ ln -s dotfiles/.bashrc .
 
 ln -s dotfiles/.bashrc_custom .
 
-if [-f .emacs.d ]; then
+if [ -f .emacs.d ]; then
   mv .emacs.d .emacs.d~
 fi
 
